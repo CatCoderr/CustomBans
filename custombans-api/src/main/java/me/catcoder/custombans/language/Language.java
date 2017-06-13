ce08f5c029917df45b0194dd294ad98a58cace8a
CatCoder <catcoderr@gmail.com>
2017-06-13 11:28:17 +0300

    Delete ColorFormat.

--- custombans-api/src/main/java/me/catcoder/custombans/language/Language.java
+++ custombans-api/src/main/java/me/catcoder/custombans/language/Language.java
@@ -19,7 +19,10 @@ import java.util.stream.Stream;
  */
 public class Language {
 
-    public static final Pattern PLACEHOLDER_MATCH = Pattern.compile("\\{([^\\}]+)\\}");
+    private static final Pattern PLACEHOLDER_MATCH = Pattern.compile("\\{([^\\}]+)\\}");
+    private static final String ALL_CODES = "0123456789AaBbCcDdEeFfKkLlMmNnOoRr";
+    private static final char COLOR_CHAR = '\u00A7';
+
 
     private Configuration configuration;
     private String fileName;
@@ -64,8 +67,15 @@ public class Language {
     }
 
     //Helpful method
-    private static String translateCodes(String input) {
-        return ColorFormat.translateAlternateColorCodes('&', input);
+    public static String translateCodes(String input) {
+        char[] b = input.toCharArray();
+        for (int i = 0; i < b.length - 1; i++) {
+            if (b[i] == '&' && ALL_CODES.indexOf(b[i + 1]) > -1) {
+                b[i] = COLOR_CHAR;
+                b[i + 1] = Character.toLowerCase(b[i + 1]);
+            }
+        }
+        return new String(b);
     }
 
     @Override
